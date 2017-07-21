056b3523fc1c1f0e41ecea795512cb20aabb491a
guym <guym@wix.com>
2017-07-21 17:47:25 +0300

    added some methods to generate proper json

--- src/main/java/com/manzurola/questions/game/GameQuestion.java
+++ src/main/java/com/manzurola/questions/game/GameQuestion.java
@@ -1,8 +1,10 @@
 package com.manzurola.questions.game;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.manzurola.questions.FillInTheBlanks;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -23,6 +25,7 @@ public class GameQuestion {
         return fillInTheBlanks.getId();
     }
 
+    @JsonIgnore
     public List<Sentence> getSentences() {
         return sentences;
     }
@@ -30,5 +33,32 @@ public class GameQuestion {
     @JsonProperty("numOfSentences")
     public int numOfSentences() {
         return sentences.size();
+    }
+
+    @JsonProperty("sentences")
+    public List<List<String>> sentences() {
+        List<List<String>> result = new ArrayList<>();
+        for (Sentence sentence : this.sentences) {
+            result.add(sentence.getParts());
+        }
+        return result;
+    }
+
+    @JsonProperty("indexOfCorrectSentence")
+    public int correctSentence() {
+        for (int i = 0; i < sentences.size(); i++) {
+            if (sentences.get(i).isCorrect()) return i;
+        }
+        return -1;
+    }
+
+    @JsonProperty("indexOfChoiceInSentences")
+    public int indexOfChoice() {
+        return sentences.get(0).getChoiceIndex();
+    }
+
+    @JsonProperty("difficulty")
+    public int difficulty() {
+        return 0;
     }
 }
